677a9e81540725fb7eed8987f3a9831b9f8dee08
jetnet <qq471723562@126.com>
2017-11-22 16:58:28 +0800

    optimal

--- longestPalindromeSubseq.c
+++ longestPalindromeSubseq.c
@@ -1,7 +1,86 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+int 
+longestMem(char *s, int **mem, int start_idx, int end_idx)
+{
+	unsigned int	len = end_idx - start_idx + 1;
+	if (len == 0) {
+		mem[start_idx][end_idx] = 0;
+		return 0;
+	}
+	if (len == 1) {
+		mem[start_idx][end_idx] = 1;
+		return 1;
+	}
+	if (mem[start_idx][end_idx] != -1) {
+		return mem[start_idx][end_idx];
+	}
+	int		max = 1;
+	for (int i = start_idx; i <= end_idx; i++) {
+		for (int j = end_idx; i < j; j--) {
+			int		temp = 0;
+			if (mem[i][j] != -1) {
+				temp = mem[i][j];
+				if (temp > max) {
+					max = temp;
+				}
+				break;
 
+			} else if (s[i] == s[j]) {
+				temp = 2;
+				int		start = i + 1;
+				int		end = j - 1;
+
+				if (end >= start) {
+					if (mem[start][ end] != -1) {
+						temp += mem[start][end];
+						printf("%d %d %d\n", mem[start][end], start, end);
+
+					} else {
+						temp += longestMem(s, mem, start, end);
+
+					}
+
+				}
+				if (temp > max) {
+					max = temp;
+				}
+				break;
+			}
+		}
+	}
+
+	mem[start_idx][end_idx] = max;
+
+	return max;
+}
+
+int 
+longestPalindromeSubseq(char *s)
+{
+
+	unsigned int	len = strlen(s);
+	if (len == 0) {
+		return 0;
+	}
+	if (len == 1) {
+		return 1;
+	}
+	int           **mem = malloc(sizeof(int *) * (len));
+
+	for (int i = 0; i < len; i++) {
+		mem[i] = malloc(sizeof(int) * len);
+		for (int j = 0; j < len; j++) {
+			mem[i][j] = -1;
+		}
+	}
+
+	return longestMem(s, mem, 0, len - 1);
+
+
+}
+/*
 int
 longestPalindromeSubseq(char *s)
 {
@@ -23,11 +102,10 @@ longestPalindromeSubseq(char *s)
 				int		temp = 2;
 
 				if (start <= end) {
-					strncpy(sub, s + start,  end - start + 1);
+					strncpy(sub, s + start, end - start + 1);
 					sub[end - start + 1] = '\0';
 					temp += longestPalindromeSubseq(sub);
 				}
-
 				if (temp > max) {
 					max = temp;
 				}
@@ -37,10 +115,12 @@ longestPalindromeSubseq(char *s)
 
 	return max;
 }
-
-int 
+*/
+int
 main()
 {
 	char           *s = "cbbd";
+	s = "gphyvqruxjmwhonjjrgumxjhfyupajxbjgthzdvrdqmdouuukeaxhjumkmmhdglqrrohydrmbvtuwstgkobyzjjtdtjroqpyusfsbjlusekghtfbdctvgmqzeybnwzlhdnhwzptgkzmujfldoiejmvxnorvbiubfflygrkedyirienybosqzrkbpcfidvkkafftgzwrcitqizelhfsruwmtrgaocjcyxdkovtdennrkmxwpdsxpxuarhgusizmwakrmhdwcgvfljhzcskclgrvvbrkesojyhofwqiwhiupujmkcvlywjtmbncurxxmpdskupyvvweuhbsnanzfioirecfxvmgcpwrpmbhmkdtckhvbxnsbcifhqwjjczfokovpqyjmbywtpaqcfjowxnmtirdsfeujyogbzjnjcmqyzciwjqxxgrxblvqbutqittroqadqlsdzihngpfpjovbkpeveidjpfjktavvwurqrgqdomiibfgqxwybcyovysydxyyymmiuwovnevzsjisdwgkcbsookbarezbhnwyqthcvzyodbcwjptvigcphawzxouixhbpezzirbhvomqhxkfdbokblqmrhhioyqubpyqhjrnwhjxsrodtblqxkhezubprqftrqcyrzwywqrgockioqdmzuqjkpmsyohtlcnesbgzqhkalwixfcgyeqdzhnnlzawrdgskurcxfbekbspupbduxqxjeczpmdvssikbivjhinaopbabrmvscthvoqqbkgekcgyrelxkwoawpbrcbszelnxlyikbulgmlwyffurimlfxurjsbzgddxbgqpcdsuutfiivjbyqzhprdqhahpgenjkbiukurvdwapuewrbehczrtswubthodv";
+	s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 	printf("%d\n", longestPalindromeSubseq(s));
 }
